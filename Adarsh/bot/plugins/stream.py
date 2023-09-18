34e41c7ad99d2097a93e3b50ae1611e3578064b2
dktest2 <120356313+dktest2@users.noreply.github.com>
2023-09-18 20:30:46 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -144,8 +144,8 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("🖥STREAM ", url=stream_link),
-                     InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ📥', url=online_link)] 
+                    [InlineKeyboardButton("◉ sᴛʀᴇᴀᴍ ◉ ", url=stream_link),
+                     InlineKeyboardButton('○ ᴅᴏᴡɴʟᴏᴀᴅ ○', url=online_link)] 
                 ]
             )
         )
